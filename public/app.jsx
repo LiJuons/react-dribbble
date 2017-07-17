9edb7e4c2dc14f1b0a37354c50d7729637789c98
lijuons <kakarie2@gmail.com>
2017-07-17 21:40:07 +0300

    Initialization

---
+++ public/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+
+
+ReactDOM.render(
+  <div>
+    <h1>Hola!</h1>
+  </div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './public/app.jsx',
+  externals: {
+    jquery: 'jQuery'
+  },
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      loader: 'babel-loader',
+      query: {
+        presets: ['react',  'es2015', 'stage-0']
+      },
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/
+    }]
+  },
+  devtool: 'inline-source-map'
+};
