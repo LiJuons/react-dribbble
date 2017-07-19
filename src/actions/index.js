4e1242d01070769bf60739cca5e27314eb577c87
adtm <tomas.eglinskas@gmail.com>
2017-07-20 00:45:27 +0300

    feat: fixed extraction error handling on redux form

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,30 +5,32 @@ const ROOT_URL = 'https://dribbble-server-dev.herokuapp.com';
 
 export function signinUser({ email, username, name, password }) {
   return function(dispatch) {
-
-    axios.post(`${ROOT_URL}/login`, { email, password })
+    axios
+      .post(`${ROOT_URL}/login`, { email, password })
       .then(response => {
         browserHistory.push('/ok');
       })
       .catch(() => {
-        console.log("Failed.");
-      })
-  }
+        console.log('Failed.');
+      });
+  };
 }
-
 
 export function signupUser({ email, username, name, password }) {
   return function(dispatch) {
-    axios.post(`${ROOT_URL}/create`, { email, username, name, password })
+    axios
+      .post(`${ROOT_URL}/create`, { email, username, name, password })
       .then(response => {
-            dispatch({ type: AUTH_USER });
-            localStorage.setItem('token', response.data.token);
-            browserHistory.push('/ok');
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/ok');
       })
-      .catch(response => {
-        dispatch(authError(response.data.error));
+      .catch(error => {
+        //TODO remove comment
+        console.log(error.response.data[0].msg);
+        dispatch(authError(error.response.data[0].msg));
       });
-  }
+  };
 }
 
 export function signoutUser() {
@@ -41,5 +43,5 @@ export function authError(error) {
   return {
     type: AUTH_ERROR,
     payload: error
-  }
+  };
 }
