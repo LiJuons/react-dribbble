99f77b811fc6e8aabd7793d8524d0963ebb81d45
lijuons <kakarie2@gmail.com>
2017-08-02 00:54:45 +0300

    Error message added to login screen

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -20,8 +20,8 @@ class Signin extends Component {
   renderAlert() {
     if (this.props.errorMessage) {
       return (
-        <div className="alert alert-danger">
-          <strong>Oops! </strong> {this.props.errorMessage}
+        <div className="notice error">
+          <h2 className="notice">We couldn’t find an account matching the username and password you entered. Please check your username and password and try again.</h2>
         </div>
       )
     }
@@ -32,33 +32,37 @@ class Signin extends Component {
 
     return (
       <div>
-        <Link to="/">
-          <img alt="Dribbble" className="center-block signin" src="https://cdn.dribbble.com/assets/signin/logo-hd-8360fbb4811d72778c637954850f3c3f0edc188a0b5e1db7041d2f4bb194dd49.png" />
-        </Link>
-        <div className="text-center signinTitle">Sign in</div>
-        <form className="signin" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <fieldset className="form-group">
-            <label className="signin">Username or Email</label>
-            <input  {...email} className="form-control auth"/ >
-            {email.touched && email.error && <div className="error">{email.error}</div>}
-          </fieldset>
-          <fieldset className="form-group">
-            <label className="signin">Password <Link to='/forgot'><p className="signin">Forgot?</p></Link></label>
-            <input type="password" {...password} className="form-control auth"/ >
-            {password.touched && password.error && <div className="error">{password.error}</div>}
-          </fieldset>
+        <div className="errorContainer">
           {this.renderAlert()}
-          <button action="submit" className="btn btn-primary signin">Sign in</button>
-          <div className="center-block signinup">Not a member? <Link to="/signup"><p className="signin">Sign Up Now</p></Link></div>
-        </form>
-        <div style={{ position: 'relative' }}>
-          <hr className="vertical"/>
-          <a className="auth-twitter" href="https://dribbble.com/auth/Twitter"> Sign in with Twitter</a>
-          <p className="blw-twitter">
-            One-click sign in to Dribbble if your account is<br/>
-            connected to Twitter. We’ll walk you through<br/>
-            connecting it if it isn’t.<br/>
-          </p>
+        </div>
+        <div className="loginContainer">
+          <Link to="/">
+            <img alt="Dribbble" className="center-block signin" src="https://cdn.dribbble.com/assets/signin/logo-hd-8360fbb4811d72778c637954850f3c3f0edc188a0b5e1db7041d2f4bb194dd49.png" />
+          </Link>
+          <div className="text-center signinTitle">Sign in</div>
+          <form className="signin" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <fieldset className="form-group">
+              <label className="signin">Username or Email</label>
+              <input  {...email} className="form-control auth"/ >
+              {email.touched && email.error && <div className="error">{email.error}</div>}
+            </fieldset>
+            <fieldset className="form-group">
+              <label className="signin">Password <Link to='/forgot'><p className="signin">Forgot?</p></Link></label>
+              <input type="password" {...password} className="form-control auth"/ >
+              {password.touched && password.error && <div className="error">{password.error}</div>}
+            </fieldset>
+            <button action="submit" className="signin">Sign in</button>
+            <div className="center-block signinup">Not a member? <Link to="/signup"><p className="signin">Sign Up Now</p></Link></div>
+          </form>
+          <div style={{ position: 'relative' }}>
+            <hr className="vertical"/>
+            <a className="auth-twitter" href="https://dribbble.com/auth/Twitter"> Sign in with Twitter</a>
+            <p className="blw-twitter">
+              One-click sign in to Dribbble if your account is<br/>
+              connected to Twitter. We’ll walk you through<br/>
+              connecting it if it isn’t.<br/>
+            </p>
+          </div>
         </div>
       </div>
     );
