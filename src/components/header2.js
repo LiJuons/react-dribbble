434c8c3a7b1fac4d409ce0233cb09949503497fb
LiJuons <kakarie2@gmail.com>
2017-09-07 16:51:53 +0300

    further navigation fixes

--- src/components/header2.js
+++ src/components/header2.js
@@ -4,13 +4,29 @@ import { Link } from 'react-router';
 
 class Header2 extends Component {
   render() {
+    const links = ['Shots', 'Designers', 'Blog', 'Podcast', 'Meetups', 'Jobs', '...'];
+    const listItems = links.map((link_name) =>
+      <li className="nav-item" key={link_name}>
+        <p className="parNav">{link_name}</p>
+      </li>
+    );
     return (
       <div className="containerNav">
-        <a href="#home">Home</a>
-        <a href="#news">News</a>
-        <div className="dropdown">
-          <button className="dropbtn">Dropdown</button>
-          <div className="dropdown-content">
+        <div>
+          <Link to="/" className="navbar-brand">
+            <img alt="dribbble" className="logo" src={"../pics/dribbble_logo.png"} />
+          </Link>
+          <ul>
+            <li className="nav-item" key={2}>
+              <Link className="nav-link" to="/signup"><p className="parNav">Sign Up</p></Link>
+            </li>
+            {listItems}
+          </ul>
+          <a href="#news">News</a>
+        </div>
+        <div className="dropdownNav">
+          <button className="dropbtnNav">Dropdown</button>
+          <div className="dropdown-contentNav">
             <a href="#">Link 1</a>
             <a href="#">Link 2</a>
             <a href="#">Link 3</a>
